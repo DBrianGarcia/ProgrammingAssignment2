6b24a5d31e56cfbb840edd6d23b4d1dad5abb753
D' Brian García <brian.gcerriteno@gmail.com>
2020-04-23 13:25:38 -0500

    Creating functions to store and compute or returned the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#############################################################################################
+# FECHA: 21/04/2020
+# AUTHOR: BRIAN ARTURO GARCÍA CERRITEÑO
+# OBJECTIVE: CREATES TWO FUNCTIONS THAT STORES AND SOLVER THE INVERSE OF A GIVEN MATRIX
+#############################################################################################
 
-## Write a short comment describing this function
+## Function that creates an special matrix object that caches its own inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   m<-NULL
+   set<-function(y) {
+     x<<-y
+     m<<-NULL
+   }
+   get<-function() x
+   setInverse<-function(matrixInverse) m<<-matrixInverse
+   getInverse<-function() m
+   list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## Computes the inverse of the special matrix defined with the makeCacheMatrix Function
+## If the inverse has already been calculated (and the matrix has not changes), it just 
+## retrieves de inverse Matrix stored
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getInverse()
+        if(!is.null(m)) {
+          message("getting chached data")
+          return(m)
+        }
+        matrix<-x$get()
+        m<-solve(matrix,...)
+        x$setInverse(m)
+        m
 }
